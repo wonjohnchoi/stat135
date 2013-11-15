1b4e174e7a9214942b19e53c37b5480b8868d510
Wonjohn Choi <wonjohn.choi@gmail.com>
2013-11-14 23:24:13 -0800

    Added data for lab2 and code for importing the data.

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -1,2 +1,12 @@
 ### Wonjohn Choi (SID: 23123143, GSI: Nanyu)
 
+long=read.csv("long.csv")
+short=read.csv("short.csv")
+medium=read.csv("medium.csv")
+
+### c. What are the approximate variances of the mle and the method of moments estimate?
+# Since we just need to calculate an "approximate" variances, we simply generate a lot of samples of size 1 from Rayleigh distributions and use them to compute mle's and mom's and calculate their variances.
+# Generate a sample from Rayleigh distribution
+genSamRay = function() {
+  
+}
