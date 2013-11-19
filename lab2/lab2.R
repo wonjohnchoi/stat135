5e0d9cc763c3ef13817dcff482962202ea5bdbf1
Wonjohn Choi <wonjohn.choi@gmail.com>
2013-11-18 16:06:48 -0800

    Finished up to g.

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -17,46 +17,59 @@ points = seq(0.1, 10, 0.01)
 mle_y = sapply(points, lik(short))
 plot(points, mle_y, type="l") # PLOT
 mle_plot = points[which(mle_y==max(mle_y))] # MLE FROM PLOT: 1.12
-mle_a = sqrt(sum(short^2)/(2*length(short))) # MLE FROM a): 1.117
+mle_short = sqrt(sum(short^2)/(2*length(short))) # MLE FROM a): 1.117
 var_c= (sum(short^2)/length(short)) /(8*length(short)) # APPROX. VARIANCE FROM c) # 0.00329
 
 # For medium,
 mle_y = sapply(points, lik(medium))
 plot(points, mle_y, type="l") # PLOT
 mle_plot = points[which(mle_y==max(mle_y))] # MLE FROM PLOT: 2.08
-mle_a = sqrt(sum(medium^2)/(2*length(medium))) # MLE FROM a): 2.076
+mle_medium = sqrt(sum(medium^2)/(2*length(medium))) # MLE FROM a): 2.076
 var_c= (sum(medium^2)/length(medium)) /(8*length(medium)) # APPROX. VARIANCE FROM c): 0.00434
 
 # For long,
 mle_y = sapply(points, lik(long))
 plot(points, mle_y, type="l") # PLOT
 mle_plot = points[which(mle_y==max(mle_y))] # MLE FROM PLOT: 3.32
-mle_a = sqrt(sum(long^2)/(2*length(long))) # MLE FROM a): 3.324
+mle_long = sqrt(sum(long^2)/(2*length(long))) # MLE FROM a): 3.324
 var_c= (sum(long^2)/length(long)) /(8*length(long)) # APPROX. VARIANCE FROM c): 0.0211
 
-### e
+### e TODO(wonjohn): fix something to be different from wonhee (for d too)
 # For short,
-mom_b = sqrt(2/pi)/length(short)*sum(short) # MOM FROM b): 1.1637
-var_c = (2/pi)*(4-pi)/(pi*length(short))*(sum(short)/length(short))^2 # APPROX. VARIANCE FROM c): 0.0039
+mom_short = sqrt(2/pi)/length(short)*sum(short) # MOM FROM b): 1.1637
+var_short = (2/pi)*(4-pi)/(pi*length(short))*(sum(short)/length(short))^2 # APPROX. VARIANCE FROM c): 0.0039
 
 # For medium,
-mom_b = sqrt(2/pi)/length(medium)*sum(medium) # MOM FROM b): 2.069
-var_c = (2/pi)*(4-pi)/(pi*length(medium))*(sum(medium)/length(medium))^2 # APPROX. VARIANCE FROM c): 0.00472
+mom_medium = sqrt(2/pi)/length(medium)*sum(medium) # MOM FROM b): 2.069
+var_medium = (2/pi)*(4-pi)/(pi*length(medium))*(sum(medium)/length(medium))^2 # APPROX. VARIANCE FROM c): 0.00472
 
 # For long,
-mom_b = sqrt(2/pi)/length(long)*sum(long) # MOM FROM b): 3.4124
-var_c = (2/pi)*(4-pi)/(pi*length(long))*(sum(long)/length(long))^2 # APPROX. VARIANCE FROM c): 0.02429
+mom_long = sqrt(2/pi)/length(long)*sum(long) # MOM FROM b): 3.4124
+var_long = (2/pi)*(4-pi)/(pi*length(long))*(sum(long)/length(long))^2 # APPROX. VARIANCE FROM c): 0.02429
 
 ### f
-rayleigh = function(theta, r) {
-  r/theta^2 * exp(-r^2/(2 * theta^2))
+# ralyeigh function which return a function that computes value of rayleigh(theta) for its input
+rayleigh = function(theta) {
+  function(r) {
+    r/theta^2 * exp(-r^2/(2 * theta^2))
+  }
 }
+
+# For short,
 hist(short, freq=F) # Histogram FOR SHORT
 points = seq(0.1, 10, 0.01)
-ray_y = rayleigh(points, rayleigh(mom_short))
-lines(points, ray_y)
+y_mom = sapply(points, rayleigh(mom_short))
+y_mle = sapply(points, rayleigh(mle_short))
+lines(points, y_mom, col="red") # plot MOM
+lines(points, y_mle, col="blue") # plot MLE
+
+# TODO(wonjohn): medium, long, legend, main
 
 ### g
+# There seems to be a relationship between my estimates
+# and the genomic separation of the point
+# because rayleigh plot with estimated paramters
+# fit the histogram very well.
 
 ### h
 genSamRay = function(theta, n) {
