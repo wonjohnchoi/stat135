a3fa259b8cb191bad30aaa8c26ca830de5be1510
Wonjohn Choi <wonjohn.choi@gmail.com>
2013-12-07 00:00:30 -0800

    Did a little more for #39 (used lm).

--- lab3/lab3.R
+++ lab3/lab3.R
@@ -13,24 +13,33 @@ plot(v, p, main="Pressure Versus Volume", xlab="Volume(kiloliters)", ylab="Press
 ### b
 # Calculate the linear regression of pressure on volume, and plot the residuals
 versus volume. What does the residual plot show?
-y = p
-x = v
 # Linear regression
-B1 = sum((x-mean(x))*(y-mean(y)))
-B0 = mean(y)-B1*mean(x)
+fit = lm(p~v)
+# B1 (slope): 2316.5
+# B0 (intercept): -257.3
 
 # Residuals
-e = y-B0-B1*x
+e = residuals(fit)
 
 # Residuals versus volume
-plot(v, e, main="Residuals Versus Volume", xlab="Volume(kiloliters)", ylab="Residuals(pascals)")
+plot(v, e, main="Residuals (Linear Fit) Versus Volume", xlab="Volume(kiloliters)", ylab="Residuals(pascals)")
 
 # The residual plot shows..
 
 
 ### c
-# Try ﬁtting pressure as a quadratic function of volume. What do you think of
-the ﬁt?
+# Try fiting pressure as a quadratic function of volume. What do you think of
+the fit?
+fit = lm(p~I(v^2)+v)
+fit
+# Coefficients:
+# (Intercept)       I(v^2)           v  
+#    -205.00        83.19      2164.03
+
+e = residuals(fit)
+# Residuals versus volume
+plot(v, e, main="Residuals (Quadratic Fit) Versus Volume", xlab="Volume(kiloliters)", ylab="Residuals(pascals)")
+# I think the fit is... 
 
 ### 43
 cysts = read.table("cysts.txt", head=T)
@@ -41,8 +50,21 @@ t25 = cysts$X25C
 n25 = cysts$n25
 t28 = cysts$X28C
 n28 = cysts$n28
-# Does the time required appear to be a linear or a quadratic function of diameter? Can you ﬁnd a model that ﬁts?
-# plot...?
+# Does the time required appear to be a linear or a quadratic function of diameter? Can you find a model that fits?
+fit10linear = lm(t10~d)
+fit10linear
+# Coefficients:
+# (Intercept)            d  
+#       384.8        -16.3  
+
+fit10quadratic = lm(t10~d + I(d^2))
+fit10quadratic
+# Coefficients:
+# (Intercept)            d       I(d^2)  
+#    776.379      -67.334        1.608
+
+
+
 
 # How do the settling rates at the three temperatures compare?
 
