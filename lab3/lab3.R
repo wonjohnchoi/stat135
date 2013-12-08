87f9668d830849c927074d57a32d9ab4699d5d3e
Wonjohn Choi <wonjohn.choi@gmail.com>
2013-12-07 23:04:13 -0800

    this commit msg is... lol

--- lab3/lab3.R
+++ lab3/lab3.R
@@ -8,6 +8,7 @@ p = vp$Pressure
 ### a
 # Plot pressure versus volume. Does the relationship appear linear?
 plot(v, p, main="Pressure Versus Volume", xlab="Volume(kiloliters)", ylab="Pressure(pascals)")
+
 # The relationship appears...
 
 ### b
@@ -15,6 +16,7 @@ plot(v, p, main="Pressure Versus Volume", xlab="Volume(kiloliters)", ylab="Press
 versus volume. What does the residual plot show?
 # Linear regression
 fit = lm(p~v)
+fit
 # B1 (slope): 2316.5
 # B0 (intercept): -257.3
 
@@ -23,7 +25,7 @@ e = residuals(fit)
 
 # Residuals versus volume
 plot(v, e, main="Residuals (Linear Fit) Versus Volume", xlab="Volume(kiloliters)", ylab="Residuals(pascals)")
-
+abline(h=0)
 # The residual plot shows..
 
 
@@ -39,6 +41,10 @@ fit
 e = residuals(fit)
 # Residuals versus volume
 plot(v, e, main="Residuals (Quadratic Fit) Versus Volume", xlab="Volume(kiloliters)", ylab="Residuals(pascals)")
+abline(h=0)
+
+plot(v, p, main="Pressure Versus Volume (Quadratic Fit)", xlab="Volume(kiloliters)", ylab="Pressure(pascals)")
+lines(v, fitted(fit))
 # I think the fit is... 
 
 ### 43
@@ -52,25 +58,25 @@ t28 = cysts$X28C
 n28 = cysts$n28
 # Does the time required appear to be a linear or a quadratic function of diameter? Can you find a model that fits?
 fit10linear = lm(t10~d)
-fit10linear
-# Coefficients:
-# (Intercept)            d  
-#       384.8        -16.3  
+plot(d, t10, main="Time(10) vs Diameter (With Linear Model Fit)", xlab="Diameter", ylab="Time")
+lines(d, fit10linear)
 
-fit10quadratic = lm(t10~d + I(d^2))
-fit10quadratic
-# Coefficients:
-# (Intercept)            d       I(d^2)  
-#    776.379      -67.334        1.608
+fit10quadratic = lm(t10~I(d^2)+d)
+plot(d, t10, main="Time(10) vs Diameter (With Quadratic Model Fit)", xlab="Diameter", ylab="Time")
+lines(d, fit10quadratic)
 
+# Repeat above two more times
+# The time required appears to be linear/quadratic function of diameter from the graphs.
 
-
+# Find the model that fits.. using average?
 
 # How do the settling rates at the three temperatures compare?
 
 
 ### 44
-read.table("asthma.txt")
-hr = read.table("cystfibr.txt", head=T)
-h = hr$height
-r = hr$resistance
+ahr = read.table("asthma.txt", head=T)
+chr = read.table("cystfibr.txt", head=T)
+ah = ahr$height
+ar = ahr$resistance
+ch = chr$height
+cr = chr$resistance
